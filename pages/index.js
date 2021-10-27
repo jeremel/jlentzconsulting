195f13f96b00b65251999ae59b72ce6f63aeaec5
jeremel <jerlentz@gmail.com>
2021-10-27 15:53:04 -0400

    Delete a bunch of images that I wasn't using from the public folder. Add alt text.

--- pages/index.js
+++ pages/index.js
@@ -134,32 +134,28 @@ export default function Home() {
 
       <main className="services" id="services">
         <div className="card">
-          {/* <img src="/Consulting1.jpg" /> */}
-          <img src="/Allura-UIWindows.png" />
+          <img src="/Allura-UIWindows.png" alt="Small Business Coaching" />
           <h1><Link href='/services'>Coaching</Link></h1>
           <p>
             We work one on one with small business owners so that they can feel more confident about the decisions that they are making for their business. With over a decade of experience working directly with owners of small businesses, we are acutely aware of their challenges, concerns, paint points, and needs.
           </p>
         </div>
         <div className="card">
-          {/* <img src="/Consulting2.jpg" /> */}
-          <img src="/Brazuca-Chart.png" />
+          <img src="/Brazuca-Chart.png" alt="Financial Analysis" />
           <h1><Link href='/services'>Finances</Link></h1>
           <p>
             If you are struggling to create a financial plan or budget that actually works or are planning for a large capital expenditure in the near future, we will provide strategic advisory services to help you analyze your situation and make the most informed financial decision possible. We have managed the finances of multiple multi-million dollar small businesses for more than ten years, and will use our experience and expertise to create a plan that works for you.
           </p>
         </div>
         <div className="card">
-          {/* <img src="/Consulting3.jpg" /> */}
-          <img src="/Amigos-Graph.png" />
+          <img src="/Amigos-Graph.png" alt="Operational Analysis" />
           <h1><Link href='/services'>Operations</Link></h1>
           <p>
             Running a business is hard work, and even the smallest changes often will have a profound ripple effect across your entire organization. We will work with you to create a plan to implement that will take the guesswork out of running the operations of your business. With experience in accounting, HR, operations, and executive management, we are uniquely positioned to help you achieve your goals.
           </p>
         </div>
         <div className="card">
-          {/* <img src="/Consulting4.jpg" /> */}
-          <img src="/Amigos-Workspace.png" />
+          <img src="/Amigos-Workspace.png" alt="Project Management" />
           <h1><Link href='/services'>Project Management</Link></h1>
           <p>
             Whether you already have a project in the pipeline that needs somebody dedicated to making sure it is accomplished on time and on budget or you are planning on some change in your busineess but do not have the time to manage it yourself, we are available to manage your projects in a way that will keep all stakeholders involved and up to date through each step of the process. We believe that our use of clear communication, our attention to detail, and our genuine desire to see our clients succeed make us the best choice to manage your project.
