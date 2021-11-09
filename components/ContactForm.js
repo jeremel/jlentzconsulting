099f935751efccdc0d9714f1b38ca08bb03e6157
jeremel <jerlentz@gmail.com>
2021-11-09 13:17:35 -0500

    Got the form working... not thrilled with it but it works and it's not like I'm expecting a ton of activity on it anyway. Will try different solutions with future website builds.

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,17 +1,25 @@
-import styled from 'styled-components'
+import { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
-import useForm from '../lib/useForm';
-import { useState } from 'react';
+import styled from 'styled-components'
 
 const USER_ID = process.env.NEXT_PUBLIC_EMAILJS_USERID;
 const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATEID;
 const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICEID;
 
-
 const FormStyles = styled.div`
     display: flex;
     flex-direction: column;
     color: #fff;
+
+    fieldset {
+      margin: 0;
+      padding: 0;
+      border: none;
+    }
+
+    fieldset:disabled {
+      cursor: not-allowed;
+    }
 
     form {
         display: flex;
@@ -21,30 +29,39 @@ const FormStyles = styled.div`
 
     label {
         font-size: 1.25rem;
-        font-weight: 500;
+        font-weight: 600;
     }
 
     input[type=text], select {
         width: 100%;
-        padding: 16px 20px;
-        margin: 8px 0;
+        padding: 1rem 1.25rem;
+        margin: 0.5rem 0;
         display: inline-block;
         border: 1px solid #ccc;
         border-radius: 4px;
         box-sizing: border-box;
         font-size: 1rem;
+        background-color: #fff;
+    }
+
+    input[type=text]:disabled {
+      cursor: not-allowed;
     }
 
     textarea {
         width: 100%;
         height: 100px;
-        padding: 12px 20px;
+        padding: 0.75rem 1.25rem;
         box-sizing: border-box;
         border: 2px solid #ccc;
         border-radius: 4px;
         background-color: #fff;
         resize: none;
         font-size: 1rem;
+    }
+
+    textarea:disabled {
+      cursor: not-allowed;
     }
 
     input[type=submit] {
@@ -76,108 +93,59 @@ const FormStyles = styled.div`
         font-weight: 600;
     }
 
-    fieldset {
-            border: none;
-            padding: 0;
-            margin: 0;
-        }
+    input[type=submit]:disabled {
+      cursor: not-allowed;
+    }
 
-        fieldset:disabled {
-            input[type="text"], input[type="email"], textarea {
-                background-color: rgb(237, 237, 237);
-                color: rgba(0, 0, 0, 0.6);
-            }
-
-            input[type="submit"] {
-                cursor: not-allowed;
-                background-color: rgba(10, 51, 81, 0.8);
-                color: rgba(0,212,255,1);
-            }
-        }
+    input[type=submit]:disabled:hover {
+      background-color: rgb(10,51,81);
+    }
 `;
 
-export default function ContactForm() {   
-    function sendEmail(e) {
-        e.preventDefault();
+export default function ContactForm() {
+  const form = useRef();
 
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-      }
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-      const { inputs, handleChange } = useForm({
-          name: '',
-          company: '',
-          email: '',
-          message: '',
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
       });
-      const [inputText, setInputText] = useState("Submit");
-      const [disable, setDisable] = useState(false);
+  };
 
-    return (
-        <FormStyles id="contact">
-            <form onSubmit={sendEmail}>
-                    <label htmlFor="name">Name: 
-                        <input
-                            required 
-                            type="text" 
-                            id="name" 
-                            name="name" 
-                            placeholder="Your Name"
-                            value={inputs.name}
-                            disabled={disable}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <label htmlFor="company">Company:
-                        <input 
-                            type="text" 
-                            id="company" 
-                            name="company"
-                            placeholder="Your Company Name"
-                            value={inputs.company}
-                            disabled={disable}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <label htmlFor="email">Email:
-                        <input 
-                            required
-                            type="text" 
-                            id="email" 
-                            name="email"
-                            placeholder="Your Email Address"
-                            value={inputs.email}
-                            disabled={disable}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <label htmlFor="message">Message:
-                        <textarea
-                            required
-                            id="message" 
-                            name="message"
-                            placeholder="Please write your message to us here!"
-                            value={inputs.message}
-                            disabled={disable}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <input 
-                        className="submitButton" 
-                        type="submit" 
-                        value={inputText}
-                        disabled={disable}
-                        onClick={() => {
-                            setInputText("Thank You!");
-                            setDisable(true);
-                            }
-                        }
-                    />
-            </form>
-        </FormStyles>
-    )
-}
+  const [inputText, setInputText] = useState("Submit");
+  // const [disable, setDisable] = useState(false);
+
+  return (
+    <FormStyles>
+      <fieldset>
+      <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="name" />
+
+      <label>Company</label>
+      <input type="text" name="company" />
+
+      <label>Email</label>
+      <input type="text" name="email" />
+
+      <label>Message</label>
+      <textarea name="message" />
+      <input 
+        type="submit" 
+        value={inputText}
+        
+        onClick={() => {
+          setInputText("Thank You!");
+          // setDisable(true);
+          }
+        }
+      />
+      </form>
+    </fieldset>
+    </FormStyles>
+  );
+};
